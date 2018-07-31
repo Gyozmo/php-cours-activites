6e035c6ddf52eb5b814f43694c9688b212ba5b22
Gyozmo <binantguillaume@gmail.com>
2018-07-31 16:05:07 +0200

    html

--- html/php-html.php
+++ html/php-html.php
@@ -10,6 +10,9 @@
     <?php
         //students
         $students = ['Hulk', 'Iron Man', 'Wonder Woman', 'Black Widow', 'Malicia'];
+        foreach ($students as $key => $value) {
+          echo $value.' ';
+        }
      ?>
      <ul>
        <?php //display the students here ?>
@@ -20,24 +23,37 @@
 
        <!-- Instructions : Créer la liste de jour (en chiffres), de mois (en chiffres) et d'année en PHP. -->
        <label for="day">Day</label>
-       <select  name="day"><?php //list of day ?></select>
+       <select  name="day"><?php for($day = 1; $day <= 31; $day++){?>
+       <option value="<?php $day  ?>"><?php echo $day  ?></option>
+       <?php
+       }
+       ?>
+       </select>
        <label for="month">Month</label>
-       <select  name="month"><?php //list of month ?></select>
+       <select  name="month"><?php for ($month = 1; $month<= 12; $month++){ ?>
+        <option value="<?php echo $month ?>"><?php echo $month ?></option>
+       <?php } ?>
+       </select>
        <label for="year">Year</label>
-       <select  name="year"><?php //list of year ?></select>
+       <select  name="year"><?php for($year = 1900; $year <= 2016; $year++){ ?>
+       <option value="<?php echo $year ?>"><?php echo $year ?></option>
+       <?php } ?>
+       </select>
      </form>
      <hr>
      <!-- Instruction : Afficher ce bloc que si dans l'URL il y'a une variable sexe et que ça valeur vaut "fille" -->
-     <p>
-       Je suis une fille
-     </p>
+     <?php if (isset($_GET["genre"]) && $_GET["genre"] == "fille"){ ?>     
+     <p>Je suis une fille </p>
+     <?php } else if (isset($_GET["genre"]) && $_GET["genre"] == "garcon"){ ?>
      <!-- Instruction : Afficher ce bloc que si dans l'URL il y'a une variable sexe et que ça valeur vaut "garçon" -->
      <p>
        Je suis un garçon
      </p>
+     <?php } else {?>
      <!-- Instruction : Afficher ce bloc dans les autres cas -->
      <p>
        Je suis indéfini
      </p>
+     <?php } ?>
   </body>
 </html>
